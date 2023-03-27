b6c6bb7a924e4b1cdde78d58ee1f747383c2513a
ThuyHien <thuyhiendoremi@gmail.com>
2023-03-27 14:23:54 +0700

    Add JavaVariables

---
+++ src/Test/JavaVariables.java
@@ -0,0 +1,12 @@
+package Test;
+
+public class JavaVariables {
+    public static void main(String[] args) {
+        String firstName = "Thuy";
+        String lastName = "Hien";
+        String fullName = firstName + lastName;
+        System.out.println(fullName);
+        int x = 28, y = 7, z = 2001;
+        System.out.println(x + y + z);
+    }
+}
