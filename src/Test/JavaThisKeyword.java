229622f9ada56a249a2af098444f7f061e69d4ba
ThuyHien <thuyhiendoremi@gmail.com>
2023-03-28 14:30:58 +0700

    Add JavaThisKeyword

---
+++ src/Test/JavaThisKeyword.java
@@ -0,0 +1,22 @@
+package Test;
+public class JavaThisKeyword {
+
+    static class Account{
+        int a;
+        int b;
+
+        public void setData(int a ,int b){
+            this.a = a;
+            this.b = b;
+        }
+        public void showData(){
+            System.out.println("Value of A ="+a);
+            System.out.println("Value of B ="+b);
+        }
+        public void main(String args[]){
+            Account obj = new Account();
+            obj.setData(2,3);
+            obj.showData();
+        }
+    }
+}
