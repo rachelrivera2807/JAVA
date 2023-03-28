76a4cc4225ef2ec0d41994e3237b6f2589d32b15
ThuyHien <thuyhiendoremi@gmail.com>
2023-03-28 09:08:39 +0700

    Edit JavaConvert

--- src/Test/JavaConvert.java
+++ src/Test/JavaConvert.java
@@ -13,5 +13,25 @@ public class JavaConvert {
         } catch (NumberFormatException e) {
             System.out.println("Unable to convert string to int.");
         }
+
+        int x =Integer.parseInt("7");
+        double y = Double.parseDouble("5");
+        int z = Integer.parseInt("287",16);
+
+        System.out.println(x);
+        System.out.println(y);
+        System.out.println(z);
+
+        int a = Integer.parseInt("4");
+        int b = Integer.parseInt("+8");
+        int c = Integer.parseInt("-9");
+        int d = Integer.parseInt("69", 18);
+        int e = Integer.parseInt("hien", 28);
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
     }
 }
