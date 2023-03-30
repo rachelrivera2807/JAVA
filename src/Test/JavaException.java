29bcac051e3cc572909df843771a54f12799a896
ThuyHien <thuyhiendoremi@gmail.com>
2023-03-30 10:43:30 +0700

    Add JavaException

---
+++ src/Test/JavaException.java
@@ -0,0 +1,26 @@
+package Test;
+
+public class JavaException {
+   /* public static void main(String[] args) {
+        try {
+            int[] myNumbers = {1, 2, 3};
+            System.out.println(myNumbers[10]);
+        } catch (Exception e) {
+            System.out.println("Something went wrong.");
+        } finally {
+            System.out.println("The 'try...catch' is finished.");
+        }
+    }*/
+    static void checkAge(int age) {
+        if (age < 18) {
+            throw new ArithmeticException("Access denied - You must be at least 18 years old.");
+        }
+        else {
+            System.out.println("Access granted - You are old enough!");
+        }
+    }
+    public static void main(String[] args) {
+        checkAge(17);
+        //checkAge(20);
+    }
+}
